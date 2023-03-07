d2d51d42ec1e8abb472c7cadf24c4029f4f94292
Sergey Tyryshkin <golovinsergey17@gmail.com>
2023-03-07 09:46:38 +0300

    Добавьте файлы проекта.

---
+++ SF2022User{19}Lib/Calculations.cs
@@ -0,0 +1,22 @@
+﻿namespace SF2022User_19_Lib
+{
+    public class Calculations
+    {
+        public TimeSpan[] startTimes;
+        public int[] durations;
+        public TimeSpan beginWorkingTime;
+        public TimeSpan endWorkingTime;
+        public int consultationTime;
+
+        public Calculations(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            this.startTimes = startTimes;
+            this.durations = durations;
+            this.beginWorkingTime = beginWorkingTime;
+            this.endWorkingTime = endWorkingTime;
+            this.consultationTime = consultationTime;
+        }
+
+
+    }
+}

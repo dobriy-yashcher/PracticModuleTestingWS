8b048a893b52b12ede5ed7ad5f20abeb05975fb4
Sergey Tyryshkin <golovinsergey17@gmail.com>
2023-03-09 14:41:03 +0300

    final AvailablePeriods() and write first(main) test

--- SF2022User{19}Lib/Calculations.cs
+++ SF2022User{19}Lib/Calculations.cs
@@ -22,20 +22,23 @@
             var result = new List<string>();
             int currentDuration = 0;
 
-
             for (TimeSpan checkTime = beginWorkingTime; checkTime < endWorkingTime; checkTime += TimeSpan.FromMinutes(consultationTime))
             {
                 string freeTimeInterval;
                 var checkTimeEnd = checkTime + TimeSpan.FromMinutes(consultationTime);
 
-                freeTimeInterval = $"{checkTime.Hours}:{checkTime.Minutes}-{checkTimeEnd.Hours}:{checkTimeEnd.Minutes}";
-                result.Add(freeTimeInterval);
-
-                /*if (checkTimeEnd < startTimes[currentDuration])
+                if (currentDuration < durations.Length && checkTimeEnd > startTimes[currentDuration])
                 {
-                }*/
-            }
+                    checkTime = startTimes[currentDuration] + TimeSpan.FromMinutes(durations[currentDuration]);
+                    checkTime -= TimeSpan.FromMinutes(consultationTime);
 
+                    ++currentDuration;
+                    continue;
+                }
+
+                freeTimeInterval = $"{checkTime.ToString("hh':'mm")} - {checkTimeEnd.ToString("hh':'mm")}";
+                result.Add(freeTimeInterval);
+            }                                                      ]
 
             return result.ToArray();
         }
--- SF2022User{19}Tests/CalculationsTests.cs
+++ SF2022User{19}Tests/CalculationsTests.cs
@@ -11,20 +11,47 @@ namespace SF2022User_19_Tests
     public class CalculationsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MainTest()
         {
+            var expected = new string[]
+            {
+                "08:00 - 08:30",
+                "08:30 - 09:00",
+                "09:00 - 09:30",
+                "09:30 - 10:00",
+                "11:30 - 12:00",
+                "12:00 - 12:30",
+                "12:30 - 13:00",
+                "13:00 - 13:30",
+                "13:30 - 14:00",
+                "14:00 - 14:30",
+                "14:30 - 15:00",
+                "15:40 - 16:10",
+                "16:10 - 16:40",
+                "17:30 - 18:00"
+            };
+         
             Calculations calc = new
             (
-                startTimes: null,
-                durations: null,
+                startTimes: new TimeSpan[] {
+                    new TimeSpan(10, 0, 0),
+                    new TimeSpan(11, 0, 0),
+                    new TimeSpan(15, 0, 0),
+                    new TimeSpan(15, 30, 0),
+                    new TimeSpan(16, 50, 0),
+                },
+                durations: new int[] { 60, 30, 10, 10, 40 },
                 beginWorkingTime: TimeSpan.FromHours(8),
                 endWorkingTime: TimeSpan.FromHours(18),
                 consultationTime: 30
             );
 
-            var res = calc.AvailablePeriods();
+            var actual = calc.AvailablePeriods();
 
-            Assert.IsNotNull( calc );
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
         }
     }
 }
